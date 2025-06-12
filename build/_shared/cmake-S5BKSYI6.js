4b79ff1f594de77a7d5b7f8f42f4837751f4dd5f
brian-rose <brian-rose@users.noreply.github.com>
2025-06-12 02:44:38 +0000

    Deploying to gh-pages from @ projectpythia-mystmd/cookbook-template@b082b8e2c2a2085c6fadafc171e4bacc01d36c7c 🚀

--- build/_shared/cmake-S5BKSYI6.js
+++ build/_shared/cmake-S5BKSYI6.js
@@ -1,1 +1,1 @@
-import{a}from"/cookbook-template/build/_shared/chunk-UGT3P5XQ.js";import"/cookbook-template/build/_shared/chunk-2NH4LW52.js";export default a();
+import{a}from"/projectpythia-mystmd/cookbook-template/build/_shared/chunk-UGT3P5XQ.js";import"/projectpythia-mystmd/cookbook-template/build/_shared/chunk-2NH4LW52.js";export default a();
--- build/_shared/unified-G6JWH5V5.js
+++ build/_shared/unified-G6JWH5V5.js
@@ -1,1 +1,1 @@
-import{b as a}from"/cookbook-template/build/_shared/chunk-KXDZNNUH.js";import"/cookbook-template/build/_shared/chunk-J6FHCSRC.js";import"/cookbook-template/build/_shared/chunk-2NH4LW52.js";export{a as unified};
+import{b as a}from"/projectpythia-mystmd/cookbook-template/build/_shared/chunk-KXDZNNUH.js";import"/projectpythia-mystmd/cookbook-template/build/_shared/chunk-J6FHCSRC.js";import"/projectpythia-mystmd/cookbook-template/build/_shared/chunk-2NH4LW52.js";export{a as unified};
